07111d08b9c5060a0fabea04958cc3a1ee43ecf6
VladislavMozheyko <mozheykovlad@mail.ru>
2022-03-07 14:31:34 +0300

    First commit

--- javaLessons/src/main/java/home/work2/Metod1.java
+++ javaLessons/src/main/java/home/work2/Metod1.java
@@ -2,19 +2,27 @@ package home.work2;
 
 public class Metod1 {
     public static void main(String[] args) {
-        System.out.println(res());;
+        //System.out.println(res());;
+        value();
 
     }
 
-    public static boolean res() {
+    //public static boolean res() {
+      //  int a=10;
+        //int b=7;
+        //int sum = a+b;
+        //if(sum<=20 && sum>=10){
+            //return true;
+        //}else {
+          //  return false;
+    public static void value(){
         int a=10;
         int b=7;
-        int sum = a+b;
-        if(sum<=20 && sum>=10){
-            return true;
-        }else {
-            return false;
-        }
+        int c=a+b;
+        System.out.println(c>=10&&c<=20? "true":"false");
 
     }
-}
+  }
+
+
+
--- javaLessons/src/main/java/home/work2/Metod4.java
+++ javaLessons/src/main/java/home/work2/Metod4.java
@@ -1,11 +1,16 @@
 package home.work2;
+import java.util.Scanner;
 public class Metod4 {
     public static void main(String[] args) {
         printWord();
     }
     public static void printWord() {
-       String a="Hello World!";
-     for (int b=0;b<5;b++){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Введите ваш текст: ");
+       String a=scan.nextLine();
+        System.out.print("Введите число повторений: ");
+        int b=scan.nextInt();
+     for (int i=0; i<b;i++){
          System.out.println(a);
      }
     }
