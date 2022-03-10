61a09333aa17caa985e166ab2790d3d5eee93148
VladislavMozheyko <mozheykovlad@mail.ru>
2022-03-10 07:50:52 +0300

    First commit

--- javaLessons/src/main/java/home/work3/Main.java
+++ javaLessons/src/main/java/home/work3/Main.java
@@ -2,16 +2,16 @@ package home.work3;
 
 public class Main {
     public static void main(String[] args) {
-        Сотрудник[] persArr=new Сотрудник[5];
-        persArr[0]=new Сотрудник("Булкин Александр Ваильевич ","Повар ","BulkaVasya@gmail.com ","+375336192754 ","300$ ",28);
-        persArr[1]=new Сотрудник("Кузьма Аркадий Петрович ","Баянист ","Kuza-Petrovich@mail.ru ","+37529447162345 ","400$ ",47);
-        persArr[2]=new Сотрудник("Дырко Иван Сергеевич ","Стамотолог ","Dzirko@yandex.ru ","+375292945518 ","1000$ ",51);
-        persArr[3]=new Сотрудник("Запивало Алина Александровна ","Товаровед ","AlinaZapivalo@mail.ru ","+375296771200 ","350$ ",33);
-        persArr[4]=new Сотрудник("Язь Алла Леонидовна ","Учитель ","Yazik@yahoo.com ","+375337881299 ","300$ ",45);
-        for(int i=0;i< persArr.length;i++){
-         if(persArr[i].возраст>40){
-             persArr[i].info();
-         }
+        Сотрудник[] persArr = new Сотрудник[5];
+        persArr[0] = new Сотрудник("Булкин Александр Ваильевич ", "Повар ", "BulkaVasya@gmail.com ", "+375336192754 ", "300$ ", 28);
+        persArr[1] = new Сотрудник("Кузьма Аркадий Петрович ", "Баянист ", "Kuza-Petrovich@mail.ru ", "+37529447162345 ", "400$ ", 47);
+        persArr[2] = new Сотрудник("Дырко Иван Сергеевич ", "Стамотолог ", "Dzirko@yandex.ru ", "+375292945518 ", "1000$ ", 51);
+        persArr[3] = new Сотрудник("Запивало Алина Александровна ", "Товаровед ", "AlinaZapivalo@mail.ru ", "+375296771200 ", "350$ ", 33);
+        persArr[4] = new Сотрудник("Язь Алла Леонидовна ", "Учитель ", "Yazik@yahoo.com ", "+375337881299 ", "300$ ", 45);
+        for (int i = 0; i < persArr.length; i++) {
+            if (persArr[i].возраст > 40) {
+                persArr[i].info();
+            }
         }
     }
-    }
+}
--- javaLessons/src/main/java/home/work3/Сотрудник.java
+++ javaLessons/src/main/java/home/work3/Сотрудник.java
@@ -3,18 +3,19 @@ package home.work3;
 import java.awt.event.WindowFocusListener;
 
 public class Сотрудник {
-    public String ФИО,должность,email,телефон,зарплата;
+    public String ФИО, должность, email, телефон, зарплата;
     public int возраст;
-    public Сотрудник(String ФИО,String должность,String email,String телефон,String зарплата,int возраст){
-        this.ФИО=ФИО;
-        this.должность=должность;
-        this.email=email;
-        this.телефон=телефон;
-        this.зарплата=зарплата;
-        this.возраст=возраст;
-    }
-public void   info(){
-    System.out.println(ФИО+"\n"+должность+"\n"+email+"\n"+телефон+"\n"+зарплата+"\n"+возраст+"\n");
 
-}
+    public Сотрудник(String ФИО, String должность, String email, String телефон, String зарплата, int возраст) {
+        this.ФИО = ФИО;
+        this.должность = должность;
+        this.email = email;
+        this.телефон = телефон;
+        this.зарплата = зарплата;
+        this.возраст = возраст;
+    }
+
+    public void info() {
+        System.out.println(ФИО + "\n" + должность + "\n" + email + "\n" + телефон + "\n" + зарплата + "\n" + возраст + "\n");
+    }
 }
